a4b2c04233b9d5f7c5de49d557d035f6bbaf463c
pancw <549191530@qq.com>
2017-03-22 17:12:53 +0800

    add lua logic

---
+++ server/logic/base/global.lua
@@ -0,0 +1,4 @@
+COMMON_CONST = Import("common/const.lua")
+--HTTP = Import("module/base/http.lua")
+JSON4LUA = Import("common/json.lua")
+GAME_MAIN = Import("game_main.lua")
---
+++ server/logic/game_main.lua
@@ -0,0 +1,4 @@
+
+function Init()
+	print("Lua Game Main Init.")
+end
